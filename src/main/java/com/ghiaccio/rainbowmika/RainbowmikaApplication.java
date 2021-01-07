02825b6c31acf5bdaf8b8cc87680e7def2df9a04
Ping Mamueng <ping.mamueng@goilluminate.com>
2021-01-07 14:09:21 -0600

    rainbowmika: refactor project into Spring

---
+++ src/main/java/com/ghiaccio/rainbowmika/RainbowmikaApplication.java
@@ -0,0 +1,13 @@
+package com.ghiaccio.rainbowmika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowmikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowmikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghiaccio/rainbowmika/RainbowmikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghiaccio.rainbowmika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowmikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
