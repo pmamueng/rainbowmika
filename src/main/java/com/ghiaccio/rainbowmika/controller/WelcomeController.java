231692617e2b4f9faa1f6233d5bd83686d7fb180
Ping Mamueng <pmamueng.dev@gmail.com>
2021-01-11 12:32:55 -0600

    rainbowmika: bump version to 0.0.5 and test keel

--- src/main/java/com/ghiaccio/rainbowmika/controller/WelcomeController.java
+++ src/main/java/com/ghiaccio/rainbowmika/controller/WelcomeController.java
@@ -12,9 +12,14 @@ public class WelcomeController {
     public static String version_2 = "0.0.2 - Nadeshiko!!!! Mika!!!!";
     public static String version_3 = "0.0.3 - You are cutting way too close there...";
     public static String version_4 = "0.0.4 - Rainbow Bomber!!!!";
+    public static String version_5 = "0.0.5 - I\'ll kick your ass! All right! Let\'s begin";
 
     @GetMapping("/moves")
     public String MoveSets() {
-        return version_1+"\r\n"+version_2+"\r\n"+version_3+"\r\n"+version_4;
+        return version_1+"\r\n"
+                +version_2+"\r\n"
+                +version_3+"\r\n"
+                +version_4+"\r\n"
+                +version_5+"\r\n";
     }
 }
